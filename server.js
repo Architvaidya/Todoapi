37f9f42f2d6746fdeff7dc20cf0fcc1f7183b25c
Architvaidya <archit.vaidya@gmail.com>
2017-02-18 19:47:59 -0500

    Added underscore library

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
 var todoNextId = 1;
+var _ = require('underscore');
 
 app.use(bodyParser.json());
 
 app.post('/todos', function (req, res) {
+
 	var body = req.body;
+	body = _.pick(body, 'completed', 'description');
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+
+		res.status(400).send();
+	}
+	body.description = body.description.trim();
 	body.id = todoNextId++;
 	todos.push(body);
 	res.json(todos);
@@ -26,13 +34,7 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
-
-	todos.forEach(function (todo) {
-		if (todoId === todo.id) {
-			matchedTodo = todo;
-		}
-	});
+	var matchedTodo = _.findWhere(todos, {id:todoId});
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
